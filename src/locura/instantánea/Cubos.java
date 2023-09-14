a12cdebd80f4cff46feaecc7c4856d848938262a
carlo <carlo@Edu-A515>
2023-09-14 17:21:10 -0600

    Inicio

---
+++ src/locura/instantánea/Cubos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package locura.instantánea;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author carlo
+ */
+public class Cubos {
+
+    Color cara1;
+    Color cara2;
+    Color cara3;
+    Color cara4;
+    Color cara5;
+    Color cara6;
+    
+    public Cubos() {
+        
+    }
+    
+    
+    
+}
