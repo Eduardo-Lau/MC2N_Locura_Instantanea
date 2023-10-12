4c5f8203fb8c5c84f649f284cbdd2054f8047e41
carlo <carlo@Edu-A515>
2023-10-11 23:49:47 -0600

    Proyecto Finalizado, se corrigió: - Error en algoritmo resolución

--- src/locura/instantanea/Locura_Instantánea.java
+++ src/locura/instantanea/Locura_Instantánea.java
@@ -11931,6 +11931,11 @@ public class Locura_Instantánea extends javax.swing.JFrame implements Serializa
     public boolean resolverJuegoSilent() {
         boolean G1Encontrado = false;
         boolean G2Encontrado = false;
+        int posI = 0;
+        int posC = 0;
+        int posW = 0;
+        int posR = 0;
+        
         // Grafo Gl 
         // Cubo 1
         String Cubo1arista1I;
@@ -12304,10 +12309,10 @@ public class Locura_Instantánea extends javax.swing.JFrame implements Serializa
                                 Cubo4aristaG2I = verticesCubo4[r];
                                 Cubo4aristaG2F = verticesCubo4[r + 1];
 
-                                if (!(Cubo1aristaG2I + Cubo1aristaG2F).equals(Cubo1aristaG1I + Cubo1aristaG1F)
-                                        && !(Cubo2aristaG2I + Cubo2aristaG2F).equals(Cubo2aristaG1I + Cubo2aristaG1F)
-                                        && !(Cubo3aristaG2I + Cubo3aristaG2F).equals(Cubo3aristaG1I + Cubo3aristaG1F)
-                                        && !(Cubo4aristaG2I + Cubo4aristaG2F).equals(Cubo4aristaG1I + Cubo4aristaG1F)) {
+                                if ((!(Cubo1aristaG2I + Cubo1aristaG2F).equals(Cubo1aristaG1I + Cubo1aristaG1F) || i != posI)
+                                        && (!(Cubo2aristaG2I + Cubo2aristaG2F).equals(Cubo2aristaG1I + Cubo2aristaG1F) || i != posC)
+                                        && (!(Cubo3aristaG2I + Cubo3aristaG2F).equals(Cubo3aristaG1I + Cubo3aristaG1F) || i != posW)
+                                        && (!(Cubo4aristaG2I + Cubo4aristaG2F).equals(Cubo4aristaG1I + Cubo4aristaG1F) || i != posR)) {
 
                                     System.out.println("Grafo G2: Vertical - Encontrado");
                                     G2Encontrado = true;
